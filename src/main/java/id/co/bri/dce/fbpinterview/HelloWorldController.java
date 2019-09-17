9440c25839c67dcd992876bf8076ba33e89ae678
apriliaaachi <achiaprilia.aa@gmail.com>
2019-09-17 11:43:18 +0700

    first

--- src/main/java/id/co/bri/dce/fbpinterview/HelloWorldController.java
+++ src/main/java/id/co/bri/dce/fbpinterview/HelloWorldController.java
@@ -2,24 +2,39 @@ package id.co.bri.dce.fbpinterview;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
   class Response {
+      public String message;
 
-    Response() {}
+      Response() {}
+  }
 
-    public String message;
+  class ResponseSum {
+      public int sum;
 
+      ResponseSum() {}
   }
 
   @GetMapping("/hello")
   Response print() {
-    Response response = new Response();
-    response.message = "Hello Dunia!";
-    return response;
+      Response response = new Response();
+      response.message = "Hello Dunia!";
+      return response;
+  }
+
+  @PostMapping("/sum")
+  ResponseSum sum(@RequestBody Integer[] data) {
+    ResponseSum res = new ResponseSum();
+    for (Integer value : data) {
+      res.sum += value.intValue();
+    }
+    return res;
   }
 
 }
--- src/test/java/id/co/bri/dce/fbpinterview/FbpInterviewApplicationTests.java
+++ src/test/java/id/co/bri/dce/fbpinterview/FbpInterviewApplicationTests.java
@@ -11,6 +11,7 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @RunWith(SpringRunner.class)
@@ -22,14 +23,29 @@ public class FbpInterviewApplicationTests {
 
   @Test
   public void getHelloApi() throws Exception {
-    String uri = "/hello";
-    MvcResult mvcResult = this.mockMvc.perform(get(uri)
-        .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+      String uri = "/hello";
+      MvcResult mvcResult = this.mockMvc.perform(get(uri)
+          .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
-    int status = mvcResult.getResponse().getStatus();
-    assertEquals(200, status);
-    String content = mvcResult.getResponse().getContentAsString();
-    assertEquals("{\"message\":\"Hello Dunia!\"}", content);
+      int status = mvcResult.getResponse().getStatus();
+      assertEquals(200, status);
+      String content = mvcResult.getResponse().getContentAsString();
+      assertEquals("{\"message\":\"Hello Dunia!\"}", content);
+  }
+
+  @Test
+  public void getSumApi() throws Exception {
+      String uri = "/sum";
+      MvcResult mvcResult = this.mockMvc.perform(
+              post(uri)
+              .content("[1, 2, 3]")
+              .contentType(MediaType.APPLICATION_JSON)
+              .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+      int status = mvcResult.getResponse().getStatus();
+      assertEquals(200, status);
+      String content = mvcResult.getResponse().getContentAsString();
+      assertEquals("{\"sum\":6}", content);
   }
 
 }
